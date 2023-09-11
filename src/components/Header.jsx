3b988ee45df98c7cfb42a0e4e17db62bb19cf30e
pablo-maff <pablo.maff.076@gmail.com>
2023-09-11 22:23:46 +0200

    header elements positioned

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
 export function Header() {
   return (
-    <header class="bg-white p-4">
-      <div class="container mx-auto flex">
-        <div class="w-1/3">
-          <h1 class="text-2xl text-green-800 font-semibold">Section 1</h1>
+    <header class="bg-white p-4 border border-green-500">
+      <div class="flex flex-row">
+        <div class="w-1/3 pr-4 flex justify-between items-center">
+          <h3 class="text-2xl text-green-800 font-semibold border border-blue-500">wyd-chat</h3>
+          <h6>Settings</h6>
         </div>
-        <div class="w-2/3">
-          <h1 class="text-2xl text-green-800 font-semibold">Section 2</h1>
+        <div class="w-2/3 flex justify-between items-center">
+          <div class="flex items-center">
+            <div class="mr-2">
+              <h6>Avatar</h6>
+            </div>
+            <div>
+              <h3 class="text-2xl text-green-800 font-semibold border border-red-500">User Name</h3>
+              <p>Typing...</p>
+            </div>
+          </div>
+          <div>
+            Icons
+          </div>
         </div>
       </div>
     </header>
