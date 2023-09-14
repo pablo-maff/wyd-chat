cdcca0f8ee267dd609ded00b7b4d84c3366cc8ba
pablo-maff <pablo.maff.076@gmail.com>
2023-09-14 10:53:45 +0200

    save before install

--- src/components/Chat/ChatInput.jsx
+++ src/components/Chat/ChatInput.jsx
@@ -1,6 +1,6 @@
 export function ChatInput() {
   return (
-    <div className="fixed p-2 m-4 mr-8" style={{ width: "-webkit-fill-available" }}>
+    <div className="fixed p-4 pr-8" style={{ width: "-webkit-fill-available" }}>
       <input type="text" className="w-full p-2 rounded-lg" />
     </div>
   )
