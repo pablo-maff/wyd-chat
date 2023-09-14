4c1c3042749f42b731705afb01977d5d9380586e
pablo-maff <pablo.maff.076@gmail.com>
2023-09-14 10:16:24 +0200

    rounded border

--- src/components/Chat/ChatInput.jsx
+++ src/components/Chat/ChatInput.jsx
@@ -1,7 +1,7 @@
 export function ChatInput() {
   return (
     <div className="fixed p-2 m-4 mr-8" style={{ width: "-webkit-fill-available" }}>
-      <input type="text" className="w-full p-2" />
+      <input type="text" className="w-full p-2 rounded-lg" />
     </div>
   )
 }
