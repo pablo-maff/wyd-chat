d6a27cadfbbd465ddcf0ca9286ee60c744a404fc
pablo-maff <pablo.maff.076@gmail.com>
2023-09-14 11:28:38 +0200

    auto size text working

--- src/components/Chat/ChatInput.jsx
+++ src/components/Chat/ChatInput.jsx
@@ -1,7 +1,24 @@
+import autosize from "autosize";
+import { useEffect, useRef } from "react";
+
 export function ChatInput() {
+  const textareaRef = useRef(null);
+
+  useEffect(() => {
+    // Initialize autosize on the textarea element using the ref
+    autosize(textareaRef.current);
+
+    return () => {
+      // Cleanup autosize when the component unmounts
+      autosize.destroy(textareaRef.current);
+    };
+  }, []);
+
   return (
     <div className="fixed p-4 pr-8" style={{ width: "-webkit-fill-available" }}>
-      <input type="text" className="w-full p-2 rounded-lg" />
+      <textarea
+        ref={textareaRef}
+        className="w-full p-2 rounded-lg resize-none max-h-80 overflow-y-auto h-10" />
     </div>
   )
 }
