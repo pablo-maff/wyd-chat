7cd3b223de80e9c20ee6a64702ea3eefded56ead
pablo-maff <pablo.maff.076@gmail.com>
2023-09-15 20:00:26 +0200

    database populated

--- index.js
+++ index.js
@@ -1,95 +1,54 @@
 import { faker } from "@faker-js/faker"
 
-let database = { users: [], chats: [] };
+let database = { chats: [], messages: [], users: [] };
 const threshold = 20;
 
 for (let i = 1; i <= threshold; i++) {
-  database.users.push({
-    id: i,
-    name: faker.person.fullName(),
-    lastMessage: {
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime()
-    },
-    avatar: faker.internet.avatar(),
-  });
+  const userFirstName = faker.person.firstName()
+  const userLastName = faker.person.lastName()
 
-  database.chats.push({
+  const newUser = {
     id: i,
-    messages: [{
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime(),
-      user: {
-        id: "user"
+    firstName: userFirstName,
+    lastName: userLastName,
+    fullName: `${userFirstName} ${userLastName}`,
+    profileImage: faker.image.avatar()
+  }
+
+  database.users.push(newUser)
+
+  if (database.users.length > 1) {
+    const newChatId = i + 10000
+    const newMessageId = i + 20000
+
+    for (let j = 1; j < 20; j++) {
+      const newMessage = {
+        id: newMessageId,
+        from: database.users[i - 1],
+        to: newUser,
+        text: faker.lorem.text(),
+        timestamp: faker.date.anytime(),
+        userId: database.users[i - 1].id,
+        chatId: newChatId
       }
-    },
-    {
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime(),
-      user: {
-        id: faker.database.mongodbObjectId()
+
+      database.messages.push(newMessage)
+    }
+
+    if (database.users.length > 2) {
+
+      const newChat = {
+        id: newChatId,
+        users: [database.users[i - 1], newUser],
+        lastMessages: [
+          database.messages?.find(message => message.userId === i - 1) || null,
+          database.messages?.find(message => message.userId === newUser.id) || null
+        ]
       }
-    },
-    {
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime(),
-      user: {
-        id: "user"
-      }
-    },
-    {
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime(),
-      user: {
-        id: faker.database.mongodbObjectId()
-      }
-    },
-    {
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime(),
-      user: {
-        id: faker.database.mongodbObjectId()
-      }
-    },
-    {
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime(),
-      user: {
-        id: "user"
-      }
-    },
-    {
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime(),
-      user: {
-        id: faker.database.mongodbObjectId()
-      }
-    },
-    {
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime(),
-      user: {
-        id: faker.database.mongodbObjectId()
-      }
-    },
-    {
-      id: faker.database.mongodbObjectId(),
-      message: faker.lorem.text(),
-      timestamp: faker.date.anytime(),
-      user: {
-        id: faker.database.mongodbObjectId()
-      }
-    }]
-  })
+
+      database.chats.push(newChat)
+    }
+  }
 }
 
 console.log(JSON.stringify(database));
