045b948b3867362df19e193b7b785d5e15b00f40
pablo-maff <pablo.maff.076@gmail.com>
2023-09-17 11:29:35 +0200

    message model defined

--- api/models/message.js
+++ api/models/message.js
@@ -1,13 +1,28 @@
 const mongoose = require('mongoose')
 
 const messageSchema = new mongoose.Schema({
-  content: {
+  from: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  to: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
     type: String,
     required: true,
   },
-  chatRoomID: {
-    type: String,
+  timestamp: {
+    type: Date,
     required: true,
+  },
+  chatRoomID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ChatRoom',
+    required: true
   },
 })
 
