3c89f3ab5d1b8c124f876baab5dff7ae54d336c1
kobrak1 <burakkarhan657@gmail.com>
2023-10-04 12:08:33 +0300

    message box issue

--- client/src/components/Chat/Message.jsx
+++ client/src/components/Chat/Message.jsx
@@ -10,7 +10,7 @@ export function Message({ message }) {
   const parsedMessageTime = format(parseISO(message?.timestamp), 'h:mm a')
 
   return (
-    <li className={clsx('max-w-[45%] py-2', isUserMessage ? 'self-end' : 'self-start')}>
+    <li className={clsx('max-w-[45%] flex py-2', isUserMessage ? 'self-end' : 'self-start')}>
       <p className={clsx('text-xs', isUserMessage ? 'mr-2 text-right' : 'ml-2')}>{parsedMessageTime}</p>
       <div className={clsx(isUserMessage ? 'bg-blueChat-300 text-white rounded-tl-lg' : 'rounded-tr-lg bg-white', 'rounded-bl-lg rounded-br-lg')}>
         <p className="text-sm pt-[6px] pr-[.5rem] pb-[.376rem] pl-[.625rem] ">{message?.text}</p>
