825cbfbb6828f1a910ee8998e8db48e7a1975252
Burak Karhan <114083611+kobrak1@users.noreply.github.com>
2023-10-06 17:36:28 +0300

    Burak branch (#32)
    * fixed
    
    * index.jsx file fixed in chat folder
    
    * message components modified
    
    * filter by name feature is added
    
    * filter feature added
    
    * Update index.jsx
    
    no default exports
    
    * Update ChatsList.jsx
    
    new line for tag
    
    * Update ContactsList.jsx
    
    tag in new line
    
    ---------
    
    Co-authored-by: Pablo Maffioli <92764736+pablo-maff@users.noreply.github.com>

--- client/src/components/Sidebar/ContactsList.jsx
+++ client/src/components/Sidebar/ContactsList.jsx
@@ -1,26 +1,46 @@
-import { useSelector } from 'react-redux'
-import { Contact } from '../Contact'
-
+import { useSelector } from 'react-redux';
+import { Contact } from '../Contact';
+import { useState } from 'react';
 export function ContactsList({ handleCreateChatRoom }) {
-  const { data: usersData } = useSelector(state => state.userContacts)
+  const { data: usersData } = useSelector(state => state.userContacts);
+
+  // * add a state variable for the filter
+  const [filterText, setFilterText] = useState('');
+
+  // * filtering function
+  const filteredUsers = usersData.filter((user) => {
+    const fullName = `${user?.firstName} ${user?.lastName}`.toLowerCase();
+    return fullName.includes(filterText.toLowerCase());
+  });
 
   return (
     <>
-      {usersData.map((user) => (
-        <li
-          key={user.id}
-          id='contact-list-item'
-          onClick={() => handleCreateChatRoom(user.id)}
-          className='hover:cursor-pointer hover:bg-blueChat-50 hover:rounded-lg'
-        >
-          <Contact
+      <input
+        type="text"
+        placeholder="Search by name..."
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+        className='bg-gray-200 m-3 px-3 h-7 rounded-md'
+      />
+      
+      {filteredUsers.map((user) => (
+        <>
+          <li
             key={user.id}
-            name={`${user?.firstName} ${user?.lastName}`}
-            avatar={user?.avatarPhoto}
-            showLastTimeOnline={user?.lastTimeOnline}
-          />
-        </li>
+            id='contact-list-item'
+            onClick={() => handleCreateChatRoom(user.id)}
+            className='hover:cursor-pointer hover:bg-blueChat-50 hover:rounded-lg'
+          >
+            <Contact
+              key={user.id}
+              name={`${user?.firstName} ${user?.lastName}`}
+              avatar={user?.avatarPhoto}
+              showLastTimeOnline={user?.lastTimeOnline}
+            />
+          </li> 
+          <hr /> 
+        </>
       ))}
     </>
-  )
+  );
 }
